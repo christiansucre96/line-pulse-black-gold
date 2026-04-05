eea2b71a6ba78b3d92d260fd0cb03ce7d41f0013
CHRISTIAN SUCRE <christiansucre96@gmail.com>
2026-04-04 21:18:22 -0400

    Create recharts.d.ts

---
+++ src/types/recharts.d.ts
@@ -0,0 +1,10 @@
+declare module 'recharts' {
+  export const BarChart: any;
+  export const Bar: any;
+  export const XAxis: any;
+  export const YAxis: any;
+  export const Tooltip: any;
+  export const ResponsiveContainer: any;
+  export const ReferenceLine: any;
+  export const Cell: any;
+}
