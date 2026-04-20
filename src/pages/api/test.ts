3982d25b3cc384daea1841863c07acf275bfe65c
CHRISTIAN SUCRE <christiansucre96@gmail.com>
2026-04-20 18:04:23 -0400

    Delete src/pages/api directory

--- src/pages/api/test.ts
+++
@@ -1,6 +0,0 @@
-// src/pages/api/test.ts
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: 'API routes are working!' })
-}
